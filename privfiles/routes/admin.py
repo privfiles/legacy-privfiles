5fef51a4700910c3dd7da79a545d0a9a573e8c12
Ward <wardpearce@protonmail.com>
2021-11-08 15:51:18 +1300

    Added files view to admin page

--- privfiles/routes/admin.py
+++ privfiles/routes/admin.py
@@ -3,6 +3,7 @@ from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
 from datetime import datetime
+from typing import Any, Dict
 
 from ..resources import Config, Sessions
 from ..errors import PasswordError, FileIdError
@@ -11,10 +12,35 @@ from ..helpers.delete import delete_upload
 
 class AdminPage(HTTPEndpoint):
     async def get(self, request: Request):
+        # Should be none or mongo find query
+        tabs: Dict[str, Any] = {
+            "manage": {
+                "active": False,
+                "query": None,
+                "result": []
+            },
+            "files": {
+                "active": False,
+                "query": Sessions.mongo.files.find().sort("downloads", -1),
+                "result": []
+            }
+        }
+
+        if "tab" in request.query_params:
+            tab = request.query_params["tab"].lower()
+
+            if tab in tabs:
+                tabs[tab]["active"] = True
+                if tabs[tab]["query"] is not None:
+                    tabs[tab]["result"] = await tabs[tab]["query"].to_list(length=100)  # type: ignore
+        else:
+            tabs["manage"]["active"] = True
+
         return Config.template.TemplateResponse(
             "admin.html",
             {
                 "request": request,
+                "tabs": tabs,
                 "error": request.query_params["error"]
                 if "error" in request.query_params else None
             }
